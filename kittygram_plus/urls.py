d0c56469ffe8d0292482034e2a6e6d4a220ca9a5
Ivan Lebedev <ivan_lbd@mail.ru>
2024-04-06 17:29:30 +0300

    DRF lesson 13

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,5 +1,4 @@
 from django.urls import include, path
-
 from rest_framework.routers import DefaultRouter
 
 from cats.views import CatViewSet, LightCatViewSet, OwnerViewSet
@@ -12,4 +11,9 @@ router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
 ]
